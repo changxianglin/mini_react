d874af5caa5d7db47f2870f01136a39a5f79027a
changxianglin <281532612@qq.com>
2021-05-06 11:59:24 +0800

    commit dom

--- my-app/src/linReact/index.js
+++ my-app/src/linReact/index.js
@@ -54,6 +54,10 @@ function workLoop(deadline) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
   }
 
+  // 任务做完统一渲染
+  if (!nextUnitOfWork && workInProgressFiber) {
+    commitRoot()
+  }
   // 空闲注册下一次任务
   requestIdleCallback(workLoop)
 }
@@ -105,7 +109,59 @@ function performUnitOfWork(fiber) {
   }
 }
 
-requestIdleCallback(workLoop) 
+requestIdleCallback(workLoop)
+
+// 统一操作 dom
+function commitRoot() {
+  commitRootImpl(workInProgressRoot.child)
+  workInProgressRoot = null
+}
+
+function commitRootImpl(fiber) {
+  if (!fiber) {
+    return 
+  }
+
+  const parentDom = fiber.return.dom
+
+  if (fiber.effectTag === 'REPLACEMENT' && fiber.dom) {
+    parentDom.appendChild(fiber.dom)
+  } else if (fiber.effectTag === 'DELETION' && fiber.dom) {
+    parentDom.appendChild(fiber.dom)
+  } else if (fiber.effectTag === 'UPDATE') {
+    updateDom(fiber.dom, fiber, alternate.props, fiber, props)
+  }
+
+  // parentDom.appendChild(fiber.dom)
+
+  // 递归操作元素
+  commitRootImpl(fiber.child)
+  commitRootImpl(fiber.sibling)
+}
+
+// recondile 调和
+function updateDom(dom , pervProps, nextProps) {
+  Object.keys(pervProps)
+    .filter(name => name !== 'children')
+    .filter(name => !(name in nextProps))
+    .forEach(name => {
+      if (name.indexOf('on') === 0) {
+        dom.removeEventListener(name.substr(2).toLowerCase(), pervProps[name], false)
+      } else {
+        dom[name] = ''
+      }
+    })
+  
+    Object.keys(nextProps)
+    .filter(name => name !== 'children')
+    .forEach(name => {
+      if (name.indexOf('on') === 0) {
+        dom.addEventListener(name.substr(2).toLowerCase(), nextProps[name], false)
+      } else {
+        dom[name] = nextProps[name]
+      }
+    })
+}
 
 
 export default {
