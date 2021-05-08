5d4da54cbb883f2406ab50d65c712599e6aea5ea
changxianglin <281532612@qq.com>
2021-05-08 10:29:47 +0800

    parse jsx

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,4 +1,8 @@
 import React from './react'
 
 
+let element = React.createElement('div', { name: 'xxx' }, 'hello', React.createElement('button', {}, '123'))
+
+console.log('element', element)
+
 React.render('React Page', document.getElementById('root'))
--- my-react/src/react/index.js
+++ my-react/src/react/index.js
@@ -1,9 +1,11 @@
 import $ from 'jquery'
 import createReactUnit from './util'
+import createElement from './element'
 
 let React = {
   render,
   nextRootIndex: 0,
+  createElement,
 }
 
 
