c8615a3b59c678996283860fb14ea3bdb797b90c
changxianglin <281532612@qq.com>
2021-05-12 10:04:57 +0800

    parse filename to ast

---
+++ own/bundler.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const babylon = require('babylon')
+const traverse = require('babel-traverse').default
+
+let ID = 0
+
+function createAsset(filename) {
+  const content = fs.readFileSync(filename, 'utf-8')
+
+  const ast = babylon.parse(content, {
+    sourceType: 'module'
+  })
+
+  const dependencies = []
+
+  traverse(ast, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value)
+    }
+  })
+
+  const id = ID++
+
+  return {
+    id,
+    filename,
+    dependencies,
+  }
+}
+
+const mainAsset = createAsset('./example/entry.js')
+
+console.log(mainAsset)
